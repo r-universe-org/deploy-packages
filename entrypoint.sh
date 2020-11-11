528158a58bb15a498214b65b46c8bf5576973e32
Jeroen Ooms <jeroenooms@gmail.com>
2020-11-11 12:41:06 +0100

    Add output with deployed packages

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,3 +6,7 @@ for dir in package-*; do
     (cd "${dir}"; eval $(cat pkgdata.txt) /deploy.sh)
     echo " === DONE! ==="
 done
+
+DEPLOYED_PACKAGES=$(echo package-*)
+echo "DEPLOYED_PACKAGES: ${DEPLOYED_PACKAGES}"
+echo ::set-output name=deployed_packages::$DEPLOYED_PACKAGES
