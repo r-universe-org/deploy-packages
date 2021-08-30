7facdba3432ba941ab0ae73a677bb15a303085d9
Jeroen Ooms <jeroenooms@gmail.com>
2021-08-30 13:23:20 +0200

    Print info.json

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 ls -lR
+
 for dir in package-*; do
   echo "::group::DEPLOYING ${dir}"
   (cd "${dir}"; eval $(cat pkgdata.txt) /deploy.sh)
@@ -10,3 +11,9 @@ done
 DEPLOYED_PACKAGES=$(echo package-*)
 echo "DEPLOYED_PACKAGES: ${DEPLOYED_PACKAGES}"
 echo ::set-output name=deployed_packages::$DEPLOYED_PACKAGES
+
+# Deploy docs
+if [ -d "docs-website" ]; then;
+  cd docs-website; unzip docs.zip; rm docs.zip
+  cat */info.json
+fi
