6d4eccc232348e12c9bb375ef1ca5e0aabd1b287
Jeroen Ooms <jeroenooms@gmail.com>
2021-08-31 22:50:47 +0200

    cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,13 +13,13 @@ echo "DEPLOYED_PACKAGES: ${DEPLOYED_PACKAGES}"
 echo ::set-output name=deployed_packages::$DEPLOYED_PACKAGES
 
 # Deploy docs
-if [ -d "docs-website" ]; then
-  echo "Found docs-website"
-  cd docs-website
-  ls -ltr
-  unzip docs.zip 
-  rm docs.zip
-  cat */info.json
-fi
+# if [ -d "docs-website" ]; then
+#   echo "Found docs-website"
+#   cd docs-website
+#   ls -ltr
+#   unzip docs.zip
+#   rm docs.zip
+#   cat */info.json
+# fi
 
 exit 0
